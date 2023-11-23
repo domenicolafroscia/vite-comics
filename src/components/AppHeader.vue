4505e8b8bac10de314a9ff3d558693eb3861b9fe
domenicolafroscia <domenicolafroscia@gmail.com>
2023-11-23 16:22:55 +0100

    add: header and product list

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,109 @@
 <script>
 export default {
-
-}
+    data() {
+        return {
+            navItems: [
+                {
+                    title: "CHARACTERS",
+                    isActive: false,
+                },
+                {
+                    title: "COMICS",
+                    isActive: true,
+                },
+                {
+                    title: "MOVIES",
+                    isActive: false,
+                },
+                {
+                    title: "TV",
+                    isActive: false,
+                },
+                {
+                    title: "GAMES",
+                    isActive: false,
+                },
+                {
+                    title: "COLLECTIBLES",
+                    isActive: false,
+                },
+                {
+                    title: "VIDEOS",
+                    isActive: false,
+                },
+                {
+                    title: "FANS",
+                    isActive: false,
+                },
+                {
+                    title: "NEWS",
+                    isActive: false,
+                },
+                {
+                    title: "SHOP",
+                    isActive: false,
+                },
+            ],
+        };
+    },
+};
 </script>
 
 <template>
-  
+    <div class="container">
+        <header>
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="Logo DC">
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="item in navItems" :key="item.title"><a :class="{ 'active': item.isActive }" href="">{{ item.title }}</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
 
+
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .logo {
+        text-align: center;
+
+        img {
+            width: 80px;
+        }
+    }
+
+    ul {
+        @include flex();
+        list-style: none;
+        gap: 1rem;
+        font-size: 0.8rem;
+
+        a {
+            text-decoration: none;
+            color: black;
+            padding: 1rem;
+            padding-bottom: 2rem;
+
+            &:hover {
+                color: $primary;
+                border-bottom: 4px solid $primary;
+            }
+
+            &.active {
+                color: $primary;
+                border-bottom: 4px solid $primary;
+            }
+        }
+    }
+}
 </style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -5,9 +5,20 @@ export default {
 </script>
 
 <template>
-    
+    <main>
+        <div class="container">
+            <h2>&RightArrow; Content goes here &leftarrow;</h2>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
 
+    main {
+        background-color: black;
+
+        h2 {
+            color: white;
+        }
+    }
 </style>
